5d8e81d02187e4716d8217b1814fcfcfe1e90ce8
wonaval <wonaval@gmail.com>
2021-12-18 23:22:23 -0500

    Fixed event routes

--- routes/eventRoute.js
+++ routes/eventRoute.js
@@ -6,7 +6,7 @@ const eventController = require('../controllers/eventController')
 // Associating requests/paths with their controller functions
 eventRoutes.post('/', eventController.create)
 eventRoutes.get('/', eventController.retrieve)
-eventRoutes.delete ('/', eventController.delete)
+eventRoutes.delete ('/:id', eventController.delete)
 
 // Export routes to be able to import elsewhere
 module.exports = eventRoutes
